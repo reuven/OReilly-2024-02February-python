0a6ce0b583886e7ec3c2da14ea40d2ed2555e027
Reuven Lerner <reuven@lerner.co.il>
2024-03-07 20:49:02 +0200

    gitautosync update 59

--- menu.py
+++ menu.py
@@ -1,3 +1,13 @@
 def get_input():
     s = input('Enter something: ').strip()
     return s
+
+def get_choice(choices):
+    while True:
+        s = input(f'Choose from {choices}: ').strip()
+    
+        if s in choices:   # did the user choose something from choices?
+            return s
+    
+        print(f'{s} is invalid; try again')
+        
